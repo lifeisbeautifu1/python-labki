878e5e81d60ec5ab724ba3f88bfe0a5793c615d1
Potato <poltoradnevone@gmail.com>
2023-11-02 06:56:20 +0400

    chore: small refactor

--- MeanShift.py
+++ MeanShift.py
@@ -80,32 +80,41 @@ class MeanShift(object):
 
     def _shift_point(self, point, points, σ):
         # from http://en.wikipedia.org/wiki/Mean-shift
-        shift_x = 0.0
-        shift_y = 0.0
-        scale_factor = 0.0
 
-        for other_point in points:
-            # numerator
-            dist = euclidean_distance(point, other_point)
-            weight = self._gaussian_kernel(dist, σ)
-            shift_x += other_point[0] * weight
-            shift_y += other_point[1] * weight
-            # denominator
-            scale_factor += weight
-        shift_x = shift_x / scale_factor
-        shift_y = shift_y / scale_factor
-        return [shift_x, shift_y]
+        # numerator
+        point_weights = self._gaussian_kernel(point - points, σ)
+        tiled_weights = np.tile(point_weights, [2, 1])
+        # denominator
+        denominator = sum(point_weights)
+        shifted_point = np.multiply(tiled_weights.transpose(), points).sum(axis=0) / denominator
+        return shifted_point
+    
+        # shift_x = 0.0
+        # shift_y = 0.0
+        # scale_factor = 0.0
+
+        # for other_point in points:
+        #     # numerator
+        #     dist = euclidean_distance(point, other_point)
+        #     weight = self._gaussian_kernel(dist, σ)
+        #     shift_x += other_point[0] * weight
+        #     shift_y += other_point[1] * weight
+        #     # denominator
+        #     scale_factor += weight
+        # shift_x = shift_x / scale_factor
+        # shift_y = shift_y / scale_factor
+        # return [shift_x, shift_y]
 
     def _gaussian_kernel(self, x, σ):
         # https://en.wikipedia.org/wiki/Radial_basis_function_kernel
-        squared_euclidean_distance = np.sqrt(((x)**2).sum())
+        squared_euclidean_distance = np.sqrt(((x)**2).sum(axis=1))
         return  (1 / (σ * (2 * np.pi) ** 0.5)) * np.exp(-0.5 *((squared_euclidean_distance / σ)) ** 2)
 
 
 if __name__ == "__main__":
     # data = np.genfromtxt('data.csv', delimiter=',')
 
-    data, y = make_blobs(centers=5, n_samples=200, random_state=40)
+    data, y = make_blobs(centers=5, n_samples=1000, random_state=40)
 
     ms = MeanShift()
     original_points, shifted_points, cluster_assignments = ms.cluster(data)
